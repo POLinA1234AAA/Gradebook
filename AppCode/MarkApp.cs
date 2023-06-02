a53ae9c60205060ae41a65e413bfa259df250fb9
Polina Merezhko <112808324+POLinA1234AAA@users.noreply.github.com>
2023-06-02 18:26:20 +0300

    Add files via upload

---
+++ AppCode/MarkApp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareVVNZ.AppCode {
+  class MarkApp {
+    public List<Mark> GeMarkList() {
+      List<Mark> MarkList = new List<Mark>();
+      MarkList.Add(new Mark(1, 35, 59, "Fx", "Незадов."));
+      MarkList.Add(new Mark(1, 60, 63, "E", "Задов."));
+      MarkList.Add(new Mark(1, 64, 74, "D", "Задов."));
+      MarkList.Add(new Mark(1, 75, 81, "C", "Добре"));
+      MarkList.Add(new Mark(1, 82, 89, "B", "Добре"));
+      MarkList.Add(new Mark(1, 90, 100, "A", "Відмінно"));
+      return MarkList;
+    }
+  }
+}
+
+
+public class Mark {
+  private int _MarkId;
+  private int _MinMark;
+  private int _MaxMark;
+  private string _SimbolMark;
+  private string _FiveBallMark;
+
+  public Mark() {
+    _MarkId = 0;
+    _MinMark = 0;
+    _MaxMark = 0;
+    _SimbolMark = String.Empty;
+    _FiveBallMark = String.Empty;
+  }
+  public Mark(int MarkId, int MinMark, int MaxMark, string SimbolMark, string FiveBallMark) {
+    _MarkId = MarkId;
+    _MinMark = MinMark;
+    _MaxMark = MaxMark;
+    _SimbolMark = SimbolMark;
+    _FiveBallMark = FiveBallMark;
+  }
+
+  public int MarkId {
+    set { _MarkId = value; }
+    get { return _MarkId; }
+  }
+  public int MinMark {
+    set { _MinMark = value; }
+    get { return _MinMark; }
+  }
+  public int MaxMark {
+    set { _MaxMark = value; }
+    get { return _MaxMark; }
+  }
+  public string SimbolMark {
+    set { _SimbolMark = value; }
+    get { return _SimbolMark; }
+  }
+  public string FiveBallMark {
+    set { _FiveBallMark = value; }
+    get { return _FiveBallMark; }
+  }
+}
---
+++ AppCode/NamesMy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareVVNZ.AppCode
+{
+ public class NamesMy {
+
+    public class ProgramButtons {
+      public const string RequiredValidation = "*";
+      public const string ErrorValidation = "!!!";
+      public const string DeleteBtn = "Видалити";
+    }
+
+    public class ProgramComBoxMessage {
+      public const string NotAvailable = "немає";
+      public const string Choise = "-вибрати-";
+    }
+    
+    public class SizeOptins {
+      public const int MinusMDIWindowSize = 30;
+      public const int MinusSizePanel = 20;
+      public const int DeleteBtnSize = 82;
+      public const int EditBtnSize = 93;
+      public const int NumberSize = 60;
+      public const int DateTimeLogOnOf = 100;
+      public const int CheckBoxSize = 30;
+      public const int UserLogin = 110;
+      public const int Name = 190;
+      public const int FIO = 220;
+      public const int ExtendedRentName = 130;
+      public const int Categoty = 280;
+      public const int Phone = 120;
+      public const int Title = 180;
+      public const int NameSize = 300;
+      public const int Message = 380;
+      public const int Quantity = 70;
+      public const int Price = 80;
+      public const int Date = 130;
+      public const int SRU = 130;
+      public const int BoardNumber = 130;
+      public const int Names = 300;
+      public const int PlusHeightActiveForm = 70;
+      public const int Event = 500;
+      public const int ProgramName = 250;
+    }
+
+    public class NoDataNames {
+      public const string NoDataInGroups = "Не додано жодної групи";
+      public const string NoDataInUsers = "Не створено жодного користувача";
+      public const string NoDataInLogs = "Не створено жодного логу";
+      public const string NoDataInDiscipline = "Не створено жодної дисципліни";
+      public const string NoDataInRatings = "Не створено жодного рейтингу";
+      public const string NoDataInStatements = "Не створено жодної відомості";
+      public const string NoDataInStudent = "Не створено жодного студента";
+      }
+
+    public class MessageBoxExaption {
+      public const string ThisUserLoginAndUserPasswordNotExistInSystem = "Користувача з таким ім'ям або паролем не існує в системі";
+      public const string CaptionMessage = " Увага!";
+      public const string YouDontHavePermission = "У вас немає прав доступу!";
+    }
+ 
+  }
+}
+
+
